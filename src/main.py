90ac01c47239c49b8ea937b7fe28ea95fb0e44f3
X0re4ik <xoore4ik@gmail.com>
2025-01-23 22:57:38 +0500

    asdasd

--- src/main.py
+++ src/main.py
@@ -6,6 +6,12 @@ from fastapi import status
 from src.gigachat.auth import GigaChatAuth
 from src.gigachat.driver import GigaChatDriver
 
+from pydantic import BaseModel
+
+
+class GigaChatMassage(BaseModel):
+    message: str
+
 GIGA_CHAT_TOKEN = "YWUxMzc0OTMtNGU0Mi00NTJjLWJlNGUtYjE4MTAyYTJkOTVkOjNlY2FiMzFmLWI2ZjEtNDdlNy04ZTZlLTNlODZmOTVhN2ZjNQ=="
 
 app = FastAPI(
@@ -15,12 +21,11 @@ app = FastAPI(
 )
 
 
-@app.get("/api/v1/gigachat/")
-async def root(message: str):
-    print(message, '!'*100)
+@app.post("/api/v1/gigachat/")
+async def root(giga_chat_message: GigaChatMassage):
     access_token = GigaChatAuth(GIGA_CHAT_TOKEN).get_access_token()
 
-    answer = GigaChatDriver(access_token).get_answer(message)
+    answer = GigaChatDriver(access_token).get_answer(giga_chat_message.message)
 
     if answer is not None:
         return {"answer": answer, "status_code": status.HTTP_200_OK}
